3596b7122dd94db1ad657d156edacd6bdb8d99b8
adamberglund <rca.berglund@gmail.com>
2020-02-18 06:29:54 +0100

    Add project files.

---
+++ Pet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Pet
+{
+    // instans variabler med getters och privata setters
+    public string name { get;private set; }
+    public int age { get; private set; }
+    public bool isFemale { get; private set; }
+    // Tom konstruktor
+    public Pet()
+    {}
+  
+    public Pet(string name, int age, bool isFemale) {
+        
+    }
+}
